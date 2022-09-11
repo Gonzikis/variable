ef6b4187d6d55000ebf3aecfb60d5754ab662bbe
gonzi <gonzikis911@bk.ru>
2022-09-11 10:31:06 +0700

    Умножение и деление

--- src/Main.java
+++ src/Main.java
@@ -19,6 +19,26 @@ public class Main {
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend * 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog  = (int) (frog / 3.5);
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+
+
+
+
+
+
 
 
 
